8e765d344fbb96ce88ef3b0c6b700c5b77d491b3
Andrew Dunn <andrew@dunn.name>
2017-08-17 06:17:11 +1000

    Initial IPv6 16-bit octuplet experiment
    Overall performance degradation.
    
    1,552,114 → 1,446,849

--- Sources/IPAddress/IPv6Address.swift
+++ Sources/IPAddress/IPv6Address.swift
@@ -21,15 +21,22 @@
 /// - Author: Andrew Dunn.
 ///
 public struct IPv6Address: LosslessStringConvertible, Equatable {
-    fileprivate let high, low: UInt64
+    typealias SegmentOctuple = (UInt16, UInt16, UInt16, UInt16, UInt16, UInt16, UInt16, UInt16)
+    fileprivate let value: SegmentOctuple
     
     public static func ==(lhs: IPv6Address, rhs: IPv6Address) -> Bool {
-        return  lhs.high == rhs.high && lhs.low == rhs.low
+        return lhs.value.0 == rhs.value.0
+            && lhs.value.1 == rhs.value.1
+            && lhs.value.2 == rhs.value.2
+            && lhs.value.3 == rhs.value.3
+            && lhs.value.4 == rhs.value.4
+            && lhs.value.5 == rhs.value.5
+            && lhs.value.6 == rhs.value.6
+            && lhs.value.7 == rhs.value.7
     }
     
     public init() {
-        low = 0;
-        high = 0;
+        value = (0,0,0,0,0,0,0,0)
     }
     
     /// Initialises a new instance with the given values.
@@ -44,39 +51,36 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
     ///   - g: The *seventh* component of the IP address.
     ///   - h: The *eighth* component of the IP address.
     public init (parts a: UInt16, _ b: UInt16, _ c: UInt16, _ d: UInt16,
-                     _ e: UInt16, _ f: UInt16, _ g: UInt16, _ h: UInt16) {
-        high = UInt64(a.bigEndian) | (UInt64(b.bigEndian) << 16)
-               | (UInt64(c.bigEndian) << 32) | (UInt64(d.bigEndian) << 48)
-        low = UInt64(e.bigEndian) | (UInt64(f.bigEndian) << 16)
-              | (UInt64(g.bigEndian) << 32) | (UInt64(h.bigEndian) << 48)
+                 _ e: UInt16, _ f: UInt16, _ g: UInt16, _ h: UInt16) {
+        value = (a.bigEndian, b.bigEndian, c.bigEndian, d.bigEndian,
+                 e.bigEndian, f.bigEndian, g.bigEndian, h.bigEndian)
     }
     
     public init?(_ str: String) {
         var segments: [UInt16] = []
         var zeroRunIndex: Int = -1
-        var currentValue: UInt32 = 0
+        var currentValue: UInt16 = 0
         var currentLength = 0
         var hasHex = false
         var wasColon = false
         var parsingQuad = false
-        var segment: UInt64 = 0
-        var hi: UInt64 = 0
-        var lo: UInt64 = 0
-        var power: UInt32 = 16
+        var segment: UInt16 = 0
+        var valBuf: SegmentOctuple = (0,0,0,0,0,0,0,0)
+        var power: UInt16 = 16
         var ipv4: UInt32 = 0
         var ipv4Shift: UInt32 = 0
         
         for c in str.unicodeScalars {
-            let val: UInt32
+            let val: UInt16
             if c >= "0" && c <= "9" {
-                val = c.value - UnicodeScalar("0")!.value
+                val = UInt16(c.value - UnicodeScalar("0")!.value)
                 currentLength += 1
                 wasColon = false
                 currentValue *= power
                 currentValue += val
             }
             else if c >= "A" && c <= "F" {
-                val = c.value - UnicodeScalar("A")!.value + 10
+                val = UInt16(c.value - UnicodeScalar("A")!.value) + 10
                 currentLength += 1
                 hasHex = true
                 wasColon = false
@@ -84,7 +88,7 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
                 currentValue += val
             }
             else if c >= "a" && c <= "f" {
-                val = c.value - UnicodeScalar("a")!.value + 10
+                val = UInt16(c.value - UnicodeScalar("a")!.value) + 10
                 currentLength += 1
                 hasHex = true
                 wasColon = false
@@ -101,7 +105,7 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
                     return nil
                 }
                 if !parsingQuad {
-                    var newV: UInt32 = 0
+                    var newV: UInt16 = 0
                     // Convert hex to dec
                     if currentValue > 0x100 {
                         newV += ((currentValue & 0xF00) >> 8) * 100
@@ -120,7 +124,7 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
                 parsingQuad = true
                 hasHex = false
                 
-                ipv4 |= currentValue << ipv4Shift
+                ipv4 |= UInt32(currentValue) << ipv4Shift
                 currentValue = 0
                 ipv4Shift += 8
                 if (ipv4Shift > 24) {
@@ -144,16 +148,27 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
                 hasHex = false
                 
                 if (zeroRunIndex == -1) {
-                    let shift: UInt64 = (segment & 0b11) << 4
-                    // Same as dividing by 4.
-                    if segment >> 2 == 0 {
-                        hi |= UInt64(UInt16(currentValue).bigEndian) << shift
-                    } else {
-                        lo |= UInt64(UInt16(currentValue).bigEndian) << shift
+                    switch segment {
+                    case 0:
+                        valBuf.0 = currentValue.bigEndian
+                    case 1:
+                        valBuf.1 = currentValue.bigEndian
+                    case 2:
+                        valBuf.2 = currentValue.bigEndian
+                    case 3:
+                        valBuf.3 = currentValue.bigEndian
+                    case 4:
+                        valBuf.4 = currentValue.bigEndian
+                    case 5:
+                        valBuf.5 = currentValue.bigEndian
+                    case 6:
+                        valBuf.6 = currentValue.bigEndian
+                    default:
+                        valBuf.7 = currentValue.bigEndian
                     }
                     segment += 1
                 } else {
-                    segments.append(UInt16(currentValue).bigEndian)
+                    segments.append(currentValue.bigEndian)
                 }
                 currentValue = 0
                 currentLength = 0
@@ -174,11 +189,12 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
                 // Part was too long.
                 return nil
             }
-            ipv4 |= currentValue << 24
+            ipv4 |= UInt32(currentValue) << 24
             
             if (zeroRunIndex == -1) {
                 segment += 2
-                lo |= UInt64(ipv4 & 0xFFFF_FFFF) << 32
+                valBuf.6 = UInt16(ipv4 & 0xFFFF)
+                valBuf.7 = UInt16((ipv4 & 0xFFFF_0000) >> 16)
             } else {
                 segments.append(UInt16(ipv4 & 0xFFFF))
                 segments.append(UInt16((ipv4 & 0xFFFF_0000) >> 16))
@@ -186,14 +202,25 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
             currentValue = 0
             currentLength = 0
         }
-
+        
         if (!parsingQuad && zeroRunIndex == -1) {
-            let shift: UInt64 = (segment & 0b11) << 4
-            // Same as dividing by 4.
-            if segment >> 2 == 0 {
-                hi |= UInt64(UInt16(currentValue).bigEndian) << shift
-            } else {
-                lo |= UInt64(UInt16(currentValue).bigEndian) << shift
+            switch segment {
+            case 0:
+                valBuf.0 = currentValue.bigEndian
+            case 1:
+                valBuf.1 = currentValue.bigEndian
+            case 2:
+                valBuf.2 = currentValue.bigEndian
+            case 3:
+                valBuf.3 = currentValue.bigEndian
+            case 4:
+                valBuf.4 = currentValue.bigEndian
+            case 5:
+                valBuf.5 = currentValue.bigEndian
+            case 6:
+                valBuf.6 = currentValue.bigEndian
+            default:
+                valBuf.7 = currentValue.bigEndian
             }
             segment += 1
         } else if currentLength > 0 || segments.count > 0 {
@@ -207,12 +234,23 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
                 segment += 1
             }
             for val in segments {
-                let shift: UInt64 = (segment & 0b11) << 4
-                // Same as dividing by 4.
-                if segment >> 2 == 0 {
-                    hi |= UInt64(val) << shift
-                } else {
-                    lo |= UInt64(val) << shift
+                switch segment {
+                case 0:
+                    valBuf.0 = val
+                case 1:
+                    valBuf.1 = val
+                case 2:
+                    valBuf.2 = val
+                case 3:
+                    valBuf.3 = val
+                case 4:
+                    valBuf.4 = val
+                case 5:
+                    valBuf.5 = val
+                case 6:
+                    valBuf.6 = val
+                default:
+                    valBuf.7 = val
                 }
                 segment += 1
             }
@@ -227,8 +265,7 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
             return nil
         }
         
-        high = hi
-        low = lo
+        value = valBuf
     }
     
     /// Returns `true` if the IP address is an unspecified, if you listen on
@@ -237,50 +274,52 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
     /// - Note: Equivalent to checking if the IP address is equal to
     ///         **::**.
     public var isUnspecified: Bool {
-        return high == 0 && low == 0
+        return value.0 == 0 && value.1 == 0 && value.2 == 0 && value.3 == 0
+            && value.4 == 0 && value.5 == 0 && value.6 == 0 && value.7 == 0
     }
-
+    
     /// Returns `true` if the IP address is a loopback address.
     ///
     /// - Note: Equivalent to checking if the IP address is **::1**.
     public var isLoopback: Bool {
-        return high == 0 && low == 0x0100_0000_0000_0000
+        return value.0 == 0 && value.1 == 0 && value.2 == 0 && value.3 == 0
+            && value.4 == 0 && value.5 == 0 && value.6 == 0 && value.7 == 0x0100
     }
     
     /// Returns `true` if the IP address is a global unicast address **(2000::/3)**.
     public var isUnicastGlobal: Bool {
-        return (high & 0xE0) == 0x20
+        return (value.0 & 0xE0) == 0x20
     }
     
     /// Returns `true` if the IP address is a unique local address **(fc00::/7)**.
     public var isUnicastUniqueLocal: Bool {
-        return (high & 0xFE) == 0xFC
+        return (value.0 & 0xFE) == 0xFC
     }
     
     /// Returns `true` if the IP address is a unicast link-local address **(fe80::/10)**.
     public var isUnicastLinkLocal: Bool {
-        return (high & 0xC0FF) == 0x80FE
+        return (value.0 & 0xC0FF) == 0x80FE
     }
     
     /// Returns `true` if the IP address is a (deprecated) unicast site-local address **(fec0::/10)**.
     public var isUnicastSiteLocal: Bool {
-        return (high & 0xC0FF) == 0xC0FE
+        return (value.0 & 0xC0FF) == 0xC0FE
     }
     
     /// Returns `true` if the IP address is a multicast address **(ff00::/8)**.
     public var isMulticast: Bool {
-        return (high & 0xFF) == 0xFF
+        return (value.0 & 0xFF) == 0xFF
     }
     
     /// Returns `true` if the IP address is in the range reserved for use in documentation.
     public var isDocumentation: Bool {
-        return (high & 0xFFFF_FFFF) == 0xb80d_0120
+        return value.0 == 0x0120 && value.1 == 0xb80d
     }
     
     /// Returns a string representation of the IP address. Will display IPv4 compatible/mapped addresses
     /// correctly, and will truncate zeroes when possible.
     public var description: String {
-        var segment: UInt64 = 0
+        var segment: UInt32 = 0
         var outputSegs: [String] = [""]
         
         var isZeroRun = false
@@ -289,22 +328,23 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
         var currentOutputSeg = 0
         var longestZeroRunLength = 0
         
-        // First 64 bits are 0.
-        if high == 0 {
+        // First 5 segments are 0.
+        if value.0 == 0 && value.1 == 0 && value.2 == 0 && value.3 == 0 && value.4 == 0 {
             // These cases need special handlers to prevent them from being presented as IPv4 compatible.
-            if low == 0 {
-                return "::"
-            }
-            if low == 0x0100_0000_0000_0000 {
-                return "::1"
+            if value.5 == 0 && value.6 == 0 {
+                if value.7 == 0 {
+                    return "::"
+                }
+                else if value.7 == 0x0100 {
+                    return "::1"
+                }
             }
             
             // Check if this is an IPv4-compatible/mapped IPv6 address.
-            let ipv4Check = low & 0x0000_0000_FFFF_FFFF
-            if (ipv4Check == 0 || ipv4Check == 0xFFFF_0000) {
+            if (value.5 == 0 || value.5 == 0xFFFF) {
                 // Use dotted quads to represent the IPv4 part.
-                let ipv4 = IPv4Address(fromUInt32: UInt32(low >> 32))
-                if (ipv4Check == 0) {
+                let ipv4 = IPv4Address(fromUInt32: UInt32(value.6) | (UInt32(value.7) << 16))
+                if (value.5 == 0) {
                     return "::\(ipv4.description)"
                 }
                 return "::ffff:\(ipv4.description)"
@@ -319,15 +359,25 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
         }
         
         while segment < 8 {
-            // Calculate which 16-bit word we should be handling. (x & 0b11) << 4, is equivalent to
-            // (x % 4) * 16.
-            let shift: UInt64 = (segment & 0b11) << 4
-            let word: UInt64
-            // Same as dividing by 4.
-            if segment >> 2 == 0 {
-                word = (high >> shift) & 0xFFFF
-            } else {
-                word = (low >> shift) & 0xFFFF
+            // Calculate which 16-bit word we should be handling.
+            let word: UInt16
+            switch segment {
+            case 0:
+                word = value.0
+            case 1:
+                word = value.1
+            case 2:
+                word = value.2
+            case 3:
+                word = value.3
+            case 4:
+                word = value.4
+            case 5:
+                word = value.5
+            case 6:
+                word = value.6
+            default:
+                word = value.7
             }
             
             let isZero = (word == 0)
@@ -381,20 +431,22 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
     
     /// Returns a quad of 32-bit unsigned ints representing the IP address.
     public var words: (UInt32, UInt32, UInt32, UInt32) {
-        return (UInt32(high & 0xFFFFFFFF), UInt32((high & 0xFFFFFFFF_00000000) >> 32),
-                UInt32(low & 0xFFFFFFFF), UInt32((low & 0xFFFFFFFF_00000000) >> 32))
+        return (UInt32(value.0) | (UInt32(value.1) << 16),
+                UInt32(value.2) | (UInt32(value.3) << 16),
+                UInt32(value.4) | (UInt32(value.5) << 16),
+                UInt32(value.6) | (UInt32(value.7) << 16))
     }
     
     /// Returns an array of octets representing the parts of the IP address.
     public var octets: [UInt8] {
-        return [UInt8(high & 0xFF), UInt8((high >> 8) & 0xFF),
-                UInt8((high >> 16) & 0xFF), UInt8((high >> 24) & 0xFF),
-                UInt8((high >> 32) & 0xFF), UInt8((high >> 40) & 0xFF),
-                UInt8((high >> 48) & 0xFF), UInt8(high >> 56),
-                UInt8(low & 0xFF), UInt8((low >> 8) & 0xFF),
-                UInt8((low >> 16) & 0xFF), UInt8((low >> 24) & 0xFF),
-                UInt8((low >> 32) & 0xFF), UInt8((low >> 40) & 0xFF),
-                UInt8((low >> 48) & 0xFF), UInt8(low >> 56)]
+        return [UInt8(value.0 & 0xFF), UInt8(value.0 >> 8),
+                UInt8(value.1 & 0xFF), UInt8(value.1 >> 8),
+                UInt8(value.2 & 0xFF), UInt8(value.2 >> 8),
+                UInt8(value.3 & 0xFF), UInt8(value.3 >> 8),
+                UInt8(value.4 & 0xFF), UInt8(value.4 >> 8),
+                UInt8(value.5 & 0xFF), UInt8(value.5 >> 8),
+                UInt8(value.6 & 0xFF), UInt8(value.6 >> 8),
+                UInt8(value.7 & 0xFF), UInt8(value.7 >> 8)]
     }
     
     /// Returns an unspecified IP address.
@@ -413,3 +465,4 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
         return Static.loopbackAddress
     }
 }
+
