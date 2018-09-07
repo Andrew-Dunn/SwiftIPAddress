9bcc24a62fc5a3e3978865a747adefd5f63d8852
vkill <vkill.net@gmail.com>
2018-09-07 15:58:44 +0800

    Replace Swift._fromWellFormedCodeUnitSequence

--- Sources/IPAddress/IPv6Address.swift
+++ Sources/IPAddress/IPv6Address.swift
@@ -14,8 +14,6 @@
 // limitations under the License.
 //
 
-import struct Foundation.Data
-
 // Use lookup tables to massively improve the performance of converting IP addresses to strings.
 fileprivate let lut = ["00","01","02","03","04","05","06","07","08","09","0a","0b","0c","0d","0e","0f",
                        "10","11","12","13","14","15","16","17","18","19","1a","1b","1c","1d","1e","1f",
@@ -439,7 +437,7 @@ public struct IPv6Address: LosslessStringConvertible, Equatable {
         }
         
         // return String._fromWellFormedCodeUnitSequence(UTF8.self, input: out.prefix(ptr))
-        return String(decoding: Data(bytes: Array(out.prefix(ptr))), as: UTF8.self)
+        return String(decoding: out.prefix(ptr), as: UTF8.self)
     }
     
     /// Returns a quad of 32-bit unsigned ints representing the IP address.
